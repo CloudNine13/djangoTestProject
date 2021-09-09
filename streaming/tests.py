50a36b1f138d478cdab34afa2b9f01e26deb3a1d
Igor Dzichkovskii <Igordzich@gmail.com>
2021-09-09 12:09:24 +0200

    Add user model tests

--- streaming/tests.py
+++ streaming/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class UsersManagerTests(TestCase):
+    def test_create_user(self):
+        username = "John Doe"
+        email = 'normal@mail.com'
+        password = '123456789'
+        User = get_user_model()
+        user = User.objects.create_user(username=username, email=email, password=password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username='', password="foo")
+
