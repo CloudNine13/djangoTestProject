492e4534701656632d365ffb100da292d04850a8
Igor Dzichkovskii <Igordzich@gmail.com>
2021-09-09 19:53:21 +0200

    Add CSS Fancy Inputs for signup

--- streaming/forms.py
+++ streaming/forms.py
@@ -3,13 +3,24 @@ from django.contrib.auth import get_user_model
 
 
 class SignUpForm(forms.ModelForm):
-    username = forms.CharField(label="name", required=True)
+    name = forms.CharField(label="name", required=True)
     email = forms.CharField(label="email", required=True)
     password = forms.CharField(label="password", widget=forms.PasswordInput(), required=True)
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password')
+        fields = ('name', 'email', 'password')
+        attrs = {
+            'type': 'text',
+            'name': 'fancy-text',
+            'id': 'fancy-text',
+            'autocomplete': 'off'
+        }
+        widgets = {
+            'name': forms.TextInput(attrs=attrs),
+            'email': forms.TextInput(attrs=attrs),
+            'password': forms.TextInput(attrs=attrs)
+        }
 
 
 class LoginForm(forms.ModelForm):
